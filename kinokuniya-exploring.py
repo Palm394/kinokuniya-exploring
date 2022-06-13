c62d527d22636127a5c03df432fa16b915dde76e
Nichapat Kerkkittikul <palm.nichapat@gmail.com>
2022-06-13 17:13:32 +0700

    Search many books at once time

--- kinokuniya-exploring.py
+++ kinokuniya-exploring.py
@@ -7,16 +7,23 @@ import os
 
 load_dotenv()
 
-URL = os.getenv("URL")
-headers = {
-    'User-Agent': os.getenv("USER_AGENT")
-}
-req = requests.get(URL ,headers=headers)
-soup = BeautifulSoup(req.content ,features="html.parser")
+BOOK_ISBN_LIST = os.getenv("BOOK_ISBN").split(",")
 
-#find title name of the book
-print(soup.find("h1").string)
+def find_a_book(ISBN_of_a_book) :
+    URL = os.getenv("URL") + ISBN_of_a_book
+    headers = {
+        'User-Agent': os.getenv("USER_AGENT")
+    }
+    req = requests.get(URL ,headers=headers)
+    soup = BeautifulSoup(req.content ,features="html.parser")
 
-#find stock status
-stock_status = str(soup.find("div", class_="stock").h2.span.string)
-print(stock_status.strip())
+    #find title name of the book
+    print(soup.find("h1").string)
+
+    #find stock status
+    stock_status = str(soup.find("div", class_="stock").h2.span.string)
+    print(stock_status.strip())
+    print('--------------------')
+
+for isbn in BOOK_ISBN_LIST:
+    find_a_book(isbn)

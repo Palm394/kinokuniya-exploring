237bed02de1d82a577176a7d1b8afaf5baa42bef
Nichapat Kerkkittikul <palm.nichapat@gmail.com>
2022-07-25 14:48:56 +0700

    change kinokuniyaName && add gitignore a pycache

--- kinokuniya.py
+++ kinokuniya.py
@@ -23,9 +23,12 @@ def find_a_book(ISBN_of_a_book) :
     soup = BeautifulSoup(req.content ,features="html.parser")
     #find title name of the book
     print(soup.find("h1").string)
+    
     #find stock status
     stock_status = str(soup.find("div", class_="stock").h2.span.string)
     print(stock_status.strip())
+    price = str(soup.find("li", class_="price").span.string)
+    print(price)
     print('--------------------')
 
 for isbn in BOOK_ISBN_LIST:
